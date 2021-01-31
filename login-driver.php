b6f7e13df4b2b4c7adb83505f6441d4981d972f5
hafizhmaulanay <hafizhmy26@gmail.com>
2021-01-31 20:49:02 +0700

    update image login

--- login-driver.php
+++ login-driver.php
@@ -95,7 +95,7 @@ if (isset($_POST["login"])) {
           <div class="row align-items-center row-login">
             <div class="col-lg-6 text-center">
               <img
-                src="assets/images/login-placeholder.png"
+                src="assets/images/login-placeholder.jpg"
                 alt=""
                 class="w-50 mb-lg-none"
               />
